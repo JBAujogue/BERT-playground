018c342f121014321e959939ee743b66a0de8577
Jean-baptiste Aujogue <jb.aujogue@gmail.com>
2023-03-02 23:23:37 +0100

    change logging directory structure

--- scripts/tensorboard.bat
+++ scripts/tensorboard.bat
@@ -2,6 +2,6 @@
 call C:/Users/jb/miniconda3/Scripts/activate.bat
 cd %~dp0..
 call conda activate transformers_nlp
-tensorboard --logdir=runs
+tensorboard --logdir=logs
 call conda deactivate
 pause
